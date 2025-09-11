732f824befef7d1e4f91f2223f57b62e9b7dc73c
NikhilSobnack <nicksobnack@gmail.com>
2025-09-11 10:07:27 -0400

    Added new input action for Attack and Block using hold button system

--- Assets/Scripts/BattleLogic.cs
+++ Assets/Scripts/BattleLogic.cs
@@ -1,16 +1,7 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BattleLogic : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
